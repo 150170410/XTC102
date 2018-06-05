6e4fa40107db6842ce3216fb880e61a69f435c10
Adam Patridge <patridge@gmail.com>
2018-06-05 14:29:25 -0600

    Ex3: add Start/Completed.

---
+++ Exercise 3/Completed/TaskyUITests/AppInitializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.UITest;
+
+namespace TaskyUITests
+{
+    public static class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .DeviceSerial("{your-device-id}")
+                    .InstalledApp("com.xamarin.samples.taskydroid")
+                    .StartApp();
+            }
+
+            else if (platform == Platform.iOS)
+            {
+                return ConfigureApp
+                    .iOS
+                    .InstalledApp("com.xamarin.taskypro")
+                    .StartApp();
+            }
+
+            throw new Exception("AppInitializer: Unsupported platform " + platform);
+        }
+    }
+}
---
+++ Exercise 3/Start/TaskyUITests/AppInitializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.UITest;
+
+namespace TaskyUITests
+{
+    public static class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .ApkFile(@"../../../../../Binaries/TaskyPro/Android/com.xamarin.samples.taskyandroid.apk")
+                    .StartApp();
+            }
+
+            else if (platform == Platform.iOS)
+            {
+                return ConfigureApp
+                    .iOS
+                    .AppBundle(@"../../../../../Binaries/TaskyPro/iOS/TaskyiOS.app")
+                    .StartApp();
+            }
+
+            throw new Exception("AppInitializer: Unsupported platform " + platform);
+        }
+    }
+}
